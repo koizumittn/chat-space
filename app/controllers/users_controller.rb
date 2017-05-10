5cd264126ab46bda01cd36482f217625d2173b6a
Tetsunosuke Koizumi <tetsu@TetsunosukenoMacBook-Pro.local>
2017-05-10 19:12:04 +0900

    update処理の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,15 @@ class UsersController < Devise::RegistrationsController
   def edit
   end
 
+  def update
+    User.update(update_params)
+    redirect_to :root
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
 
