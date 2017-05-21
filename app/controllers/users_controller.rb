80bd9f8efeaac63efe77df5a4fb943238aec6a2d
Tetsunosuke Koizumi <tetsu@TetsunosukenoMacBook-Pro.local>
2017-05-21 15:14:12 +0900

    ユーザ編集機能にパスワードのチェック・変更を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,19 @@ class UsersController < Devise::RegistrationsController
   end
 
   def update
-    User.update(update_params)
-    redirect_to :root
+    if current_user.update_with_password(update_params)
+      redirect_to :root
+    else
+      # パスワードに誤りがある場合
+      redirect_to edit_user_registration_path(current_user.id)
+    end
   end
 
   private
   def update_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user)
+    .permit(:name, :email, :password, :current_password)
+    .merge(password_confirmation: params.require(:user)[:confirm_password])
   end
 
 end
